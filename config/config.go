42cd1ceb1ad3c8d9d7269f433358e3c8684a0a4f
Steven Lin <steven@bitgin.net>
2022-10-26 15:59:30 +0800

    deployed

--- config/config.go
+++ config/config.go
@@ -1,12 +1,14 @@
 package config
 
-const (
+import "os"
+
+var (
 	// Frontend_Endpoint = "<FRONTEND_ENDPOINT>"
 	// Backend_Endpoint  = "<BACKEND_ENDPOINT>"
 	// Key               = "<API_KEY>"    // API_KEY acquired from BITGIN
 	// Secret            = "<SECRET_KEY>" // SECRET_KEY acquired from BITGIN
 	Frontend_Endpoint = "https://stage.bitgin.app/fiat-as-a-service"
 	Backend_Endpoint  = "https://api.bitgin.app"
-	Key               = "6IAEPfwAgW2rjSOTdK7l"                     // API_KEY acquired from BITGIN
-	Secret            = "7pK5zkfRIaOVtUXx23JgRvPKbCP3mcQ4V7Fb9M3t" // SECRET_KEY acquired from BITGIN
+	Key               = os.Getenv("KEY")    // API_KEY acquired from BITGIN
+	Secret            = os.Getenv("SECRET") // SECRET_KEY acquired from BITGIN
 )
